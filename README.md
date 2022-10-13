bb8576bb2e67c1ad014d5afba23bd71ce2dff959
Gamal Rivaldi M <gamlrvldi5@gmail.com>
2022-10-13 13:43:05 +0700

    add readme

---
+++ README.md
@@ -0,0 +1,4 @@
+INSTALATION
+
+use yarn: yarn android:dev
+use npm: npn run android:dev
--- src/Theme/Images.ts
+++ src/Theme/Images.ts
@@ -4,9 +4,6 @@ export default function ({}: ThemeVariables) {
   return {
     ic_default: require('@/Assets/Icons/Icon/unknown.png'),
     base_logo: require('@/Assets/Icons/Logo/brand.png'),
-    ic_logo: require('@/Assets/Icons/Logo/shipdeo_logo.png'),
-    ic_home: require('@/Assets/Icons/Icon/homes-revamp.png'),
-    ic_closed_btn_blue: require('@/Assets/Icons/Icon/closed_blue.png'),
     loading_blue: require('@/Assets/Animation/custom-loading.json'),
     background: require('@/Assets/Images/background.jpg'),
     avatar: require('@/Assets/Images/avatar.png')
